e2b54559bf6660812376a829f1a9e7d145ee3b7e
RullyanBananaPi <julien.viala26@gmail.com>
2017-05-22 19:41:34 +0200

    Creation du controleur (index.php)

--- controleur/inscription.php
+++ controleur/inscription.php
@@ -5,5 +5,19 @@
  * Date: 22/05/17
  * Time: 18:54
  */
-
+    if(!empty($_POST['nom']) || !empty($_POST['prenom'])|| !empty($_POST['mail'])
+        || !empty($_POST['psw']) || !empty($_POST['pswVerif'])){
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $mail = $_POST['mail'];
+        $psw = $_POST['psw'];
+        $pswVerif = $_POST['pswVerif'];
+        if(strcmp($psw,$pswVerif)!=0){
+            echo"<script language=\"javascript\">";
+            echo"alert('bonjour')";
+            echo"</script>";
+        }
+    }else{
+        include '../controleur/index.php?page=inscription';
+    }
 ?>
