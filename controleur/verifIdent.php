223966089b2bb7e88c7b2a9d99d6db4a1f24a31e
RullyanBananaPi <julien.viala26@gmail.com>
2017-05-22 14:32:23 +0200

    Completion verifIdent et creation passagePageInscrip.php

--- controleur/verifIdent.php
+++ controleur/verifIdent.php
@@ -5,11 +5,14 @@
  * Date: 22/05/17
  * Time: 11:03
  */
+
     if (!empty($_POST)) {
         foreach ($_POST as $key => $value){
             if ($key == "mail"){
+                global $login;
                 $login = $value;
             }elseif ($key == "psw"){
+                global $psw;
                 $psw = $value;
             }
         }
