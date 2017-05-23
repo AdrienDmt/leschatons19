ae38e713e4fb43d69bc90836dd51df7cea6dbd66
RullyanBananaPi <julien.viala26@gmail.com>
2017-05-23 23:30:59 +0200

    Creation du controleur (index.php)

--- controleur/inscription.php
+++ controleur/inscription.php
@@ -26,7 +26,7 @@
         try{
             $dao->createUtilisateur($util);
         }catch(Exception $e){
-            echo "DEBUG inscription.php: ".$e->getMessage();
+            echo "DEBUG test inscription.php: ".$e->getMessage();
         }
 
         echo"<script language=\"javascript\">";
