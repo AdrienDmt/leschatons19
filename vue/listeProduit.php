0a41ef47ec246bf791aa3b98cd423af8875f5704
RullyanBananaPi <julien.viala26@gmail.com>
2017-05-23 15:04:16 +0200

    Creation du controleur (index.php)

--- vue/listeProduit.php
+++ vue/listeProduit.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="../vue/accueil.css" media="screen" title="no title" >
   <link rel="stylesheet" href="../vue/listeProduit.css">
 </head>
-<body>
+<body onload="init()">
   <header>
     <div id="Banderole">
       <h1><a href="../controleur/index.php?page=accueil"><img src="../data/logo-chat.png" alt="logo" id="logo"/></a> Bienvenue sur le site des chatons</h1>
@@ -118,5 +118,6 @@
   <footer>
     <p>Le projet web de Julien, Brenda, Alexandre et Raphael.</p>
   </footer>
+    <script src="../controleur/constPageVisu.js"
 </body>
 </html>
