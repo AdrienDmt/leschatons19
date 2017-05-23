2f00f89970d3e8a4346ac97437b9b00b78693899
Brenda Hoareau <hoareau.brenda@gmail.com>
2017-05-23 13:31:11 +0200

    creation page description
    Màj lienChat

---
+++ vue/descriptionProduit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Description</title>
+  </head>
+  <body>
+    <header>
+      <div id="Banderole">
+        <h1><a href="../controleur/index.php?page=accueil"><img src="../data/logo-chat.png" alt="logo" id="logo"/></a> Bienvenue sur le site des chatons</h1>
+      </div>
+    </header>
+  </body>
+</html>
