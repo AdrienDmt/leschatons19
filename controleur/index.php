b49425371fff485ea73e4b01b2df31f18380c33e
RullyanBananaPi <julien.viala26@gmail.com>
2017-05-24 08:14:06 +0200

    Creation du controleur (index.php)

--- controleur/index.php
+++ controleur/index.php
@@ -9,29 +9,36 @@
     if(!empty($_GET['page'])){
         if ($_GET['page']=='accueil'){
             include '../vue/accueil.php';
+
         }elseif ($_GET['page']=='compte'){
             if (isset($_COOKIE['connecte'])){
                 include '../vue/compte.php';
             }else{
                 include '../vue/connect.php';
             }
+
         }elseif ($_GET['page']=='inscription'){
             include '../vue/inscription.php';
+
         }elseif ($_GET['page']=='panier'){
             if(isset($_COOKIE['connecte']) && $_COOKIE['connecte']!="unregistred")
                 include '../vue/connect.php';
             else
                 include '../vue/commande.php';
+
         }elseif ($_GET['page']=='achat'){
+
 
         }elseif ($_GET['page']=='liste') {
             //if(empty($_GET['categorie']))
             //    include 'index.php?page=error';
             //else
                 include '../vue/listeProduit.php';
+
         }elseif ($_GET['page']=='deconnexion') {
             unset($_COOKIE['connecte']);
             include '../vue/accueil.php';
+
         }elseif ($_GET['page']=='error'){
 
         }else{
