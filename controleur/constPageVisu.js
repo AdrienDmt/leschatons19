aa9b1e325919fa7c8f7f3c4a38f18bd9016a9fc7
RullyanBananaPi <julien.viala26@gmail.com>
2017-05-24 13:56:33 +0200

    correction des liens

--- controleur/constPageVisu.js
+++ controleur/constPageVisu.js
@@ -3,12 +3,12 @@
  */
 
 function init(){
-    console.log("test");
+    //console.log("test");
     var categ = $_GET('categorie');
     var data = encodeURIComponent(categ);
     try{
         console.log(data);
-        ajax_get_request(placementDiv, '../controleur/recupVisuelProduit.php?categorie='+data, true);
+        ajax_get_request(placementDiv, '../controleur/recupVisuelProduit.php?categorie='+data, false);
     }catch(err){
         alert("erreur : "+err);
     }
