e87a32a67268fb0f81047f89b7e3b390ccad2a68
RullyanBananaPi <julien.viala26@gmail.com>
2017-05-31 17:36:19 +0200

    correction des liens

--- controleur/verifIdent.php
+++ controleur/verifIdent.php
@@ -28,7 +28,9 @@
         $dao=new DAO();
         $user = $dao->getUtilisateur($mail, $psw);
         if (!empty($user)) {
-                setcookie("connecte", $mail, time()+24*60*60);
+            setcookie("connecte", $mail, time()+24*60*60);
+            $_GET['page'] = "accueil";
+            include '../controleur/index.php';
                 // setcookie("nom", $user->nom);
                 // setcookie("prenom", $user->prenom);
             }
@@ -40,22 +42,14 @@
             // echo"alert('Vous n'êtes pas inscrit ou votre mot de passe n'est pas bon!')";
             // echo"</script>";
             setcookie("connect", "", time()-1);
-            setcookie("nom", "", time()-1);
-            setcookie("prenom", "", time()-1);
             unset($_COOKIE["connecte"]);
-            unset($_COOKIE["nom"]);
-            unset($_COOKIE["prenom"]);
             $_GET['page'] = "inscription";
             include '../controleur/index.php';
         }
     }else{
         // la connexion s'est mal passée, on retourne à l'accueil, sans indiquer qu'il y a eu une erreur
         setcookie("connect", '', time()-1);
-        setcookie("nom", '', time()-1);
-        setcookie("prenom", '', time()-1);
         unset($_COOKIE["connecte"]);
-        unset($_COOKIE["nom"]);
-        unset($_COOKIE["prenom"]);
 
         // NE FONCTIONNE PAS
         // echo"<script language=\"javascript\">";
